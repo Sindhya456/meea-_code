181245c31d2a7cef01b37a44f65968a549fca0fc
Sindhya456 <101663602+Sindhya456@users.noreply.github.com>
2026-01-17 18:22:32 -0600

    Update mcts.py

--- mcts.py
+++ mcts.py
@@ -260,9 +260,9 @@ if __name__ == '__main__':
     else:
         known_mols = set()
 
-    policy_model_path = 'policy_model_path = '/home/ubuntu/meea-_code/saved_model/policy_model.ckpt'
+    policy_model_path = '/home/ubuntu/meea-_code/saved_model/policy_model.ckpt'
 '
-    value_model_path = 'policy_model_path = '/home/ubuntu/meea-_code/saved_model/
+    value_model_path = '/home/ubuntu/meea-_code/saved_model/
 /value_pc.pt'
     expand_fn = prepare_expand(policy_model_path, gpu=device_list[0])
     value_model = prepare_value(value_model_path, gpu=device_list[0])
